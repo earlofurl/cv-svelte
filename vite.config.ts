e6d0cdf1608f71aab3508404a7f3592fccb0ed98
Jack Elliott <jackthename@gmail.com>
2023-01-25 20:07:01 -0800

    add vite and docker config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
+let runMode: string;
+let hostMode: boolean;
+
+if (process.env.NODE_ENV === 'production') {
+	runMode = 'prod';
+	hostMode = false;
+} else {
+	runMode = 'dev';
+	hostMode = true;
+}
+
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		host: hostMode,
+		port: 3069
 	}
 };
 
