de3efb6c4f89ba5d2c1ea9f887ae8bc3fa1b8f85
Jack Elliott <jackthename@gmail.com>
2023-01-25 19:58:03 -0800

    basic homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Jack Elliott</h1>
+<h3>Software Engineer</h3>
+<p>Visit <a href="https://www.linkedin.com/in/jackpelliott/">my LinkedIn profile</a> to learn more about me.</p>
+
+<style>
+    h1 {
+        font-size: 3rem;
+        font-weight: 700;
+        margin: 0;
+    }
+    h3 {
+        font-size: 1.5rem;
+        font-weight: 400;
+        margin: 0;
+    }
+    p {
+        font-size: 1.25rem;
+        font-weight: 400;
+        margin: 0;
+    }
+    a {
+        color: #000;
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+</style>
