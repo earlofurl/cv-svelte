c92b9f70e32c38e0fb03bb3c75266db4b273f7b4
Jack Elliott <jackthename@gmail.com>
2023-01-27 01:27:38 -0800

    added tailwind

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,22 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+src/app.d.ts,5\0\50d669c9d022d89914a4c9c77a94171b6b438948
+<
+.env.example,d\4\d4dae00d11854b35292c2b8a30515a9a0aa2d871
+B
+docker-compose.yml,3\5\35b8c13cf2eb2a194eada000eb310d65aed53b2a
+>
+vite.config.ts,7\5\75ea7f3ea6d80d54bb5b2357996972d1632a5038
+A
+src/index.test.ts,b\4\b4cfc47e8a386d8e62c52768d5c393a322e9c639
+<
+src/app.html,4\3\43f343fa8f8b3d19f33620db6bc88b192afa9d3d
+:
+
+Dockerfile,6\6\6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+;
+.prettierrc,2\0\20137a9e94744624390b6748bc96df5ca1579b71
+@
+svelte.config.js,9\4\9412e23d6c9b081f2298258aff5115cc43e3308e
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,10 @@
-<h1>Jack Elliott</h1>
-<h3>Software Engineer</h3>
-<p>Visit <a href="https://www.linkedin.com/in/jackpelliott/">my LinkedIn profile</a> to learn more about me.</p>
-
-<style>
-    h1 {
-        font-size: 3rem;
-        font-weight: 700;
-        margin: 0;
-    }
-    h3 {
-        font-size: 1.5rem;
-        font-weight: 400;
-        margin: 0;
-    }
-    p {
-        font-size: 1.25rem;
-        font-weight: 400;
-        margin: 0;
-    }
-    a {
-        color: #000;
-        text-decoration: none;
-    }
-    a:hover {
-        text-decoration: underline;
-    }
-</style>
+<div class="flex flex-col h-screen items-center">
+    <h1 class="font-bold text-4xl text-center text-gray-800 mb-4 mt-8 w-1/2">
+        Jack Elliott
+    </h1>
+    <h3 class="font-semibold text-2xl text-center text-gray-800 mb-4 w-2/3">Software Engineer</h3>
+    <p class="font-normal text-xl text-center text-gray-800 w=2/3">
+        Visit <a class="text-blue-500 hover:text-blue-700" href="https://www.linkedin.com/in/jackpelliott/">
+        my LinkedIn profile</a> to learn more about me.
+    </p>
+</div>
